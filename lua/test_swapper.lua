25f21bb060275046e76e7021855f797e369f9955
Jacob <m31k0r@users.noreply.github.com>
2026-09-25 13:50:15 +0200

    Removed Config and added global def

--- lua/test_swapper.lua
+++ lua/test_swapper.lua
@@ -1,15 +1,11 @@
 -- main module file
 local module = require("test_swapper.module")
 
----@class Config
----@field opt string Your config option
-local config = {}
-
 ---@class MyModule
 local M = {}
 
----@type Config
-M.config = config
+_G.Snacks = M
+_G.svim = require("snacks.nvim")
 
 M.swap_test_or_cpp = function()
   return module.swap_test_or_cpp()
